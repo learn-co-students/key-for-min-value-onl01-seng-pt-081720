9457e0a298b4d9d40b497b90137eeac85d39d472
William West <wwest466@gmail.com>
2020-08-25 23:49:52 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+    return nil
+  end
+
+arr2 = []
+  name_hash.map do |x,y|
+    arr2 << y
+  end
+
+arr3 = arr2.sort
+
+key = name_hash.find do |x,y|
+  y == arr3[0]
+end
+return key[0]
 end
