81f49d56492982f21a4cfb3c103f9513d87de627
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-30 19:55:29 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 def key_for_min_value(name_hash)
-  min_value = 0
+  min_value = nil
   min_key= nil
   name_hash.each do |key, value|
-    if min_value == 0 || value < min_value
+    if min_value == nil || value < min_value
       min_value = value
       min_key = key
     end
