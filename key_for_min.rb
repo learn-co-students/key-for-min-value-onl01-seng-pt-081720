7eae152e6b8d18a0112c2418e0bd7985c23a1ad6
Myles Janik <mylesjanik@gmail.com>
2020-08-31 03:30:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   counter = 0
   temp_value = 1000000
   temp_counter = 0
-  temp_key = ""
+  temp_key = nil
   
   name_hash.each do |key, value|
     if value < temp_value
