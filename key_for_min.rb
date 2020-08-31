e1f91d6e9b62c1a0de882c3a9e753c0cdeb71427
Myles Janik <mylesjanik@gmail.com>
2020-08-31 03:22:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  counter = 0
+  temp_value = 0
+  temp_counter = 0
+  
+  name_hash.each do |key, value|
+    if value > temp_value
+      temp_value = value
+    else
+      temp_counter = counter
+    end
+    counter += 1
+  end
+  
+  counter = 0
+  
+  name_hash.collect do |key, value|
+    if counter == temp_counter
+      return key
 end
